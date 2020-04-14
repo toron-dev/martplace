ecb2e92fa98bd1528000ffca278ab877888fd3dd
toron-dev <toron1988sata@gmail.com>
2020-04-14 22:04:17 +0300

    added slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,15 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 4.6,
+        starWidth: "17px",
+        readOnly: true
+      });
 
-
-    
+      $('.week__slider-inner').slick({
+        infinite: true,
+        slidesToShow: 1,
+      });
+      
+      var mixer = mixitup('.products__inner-box')
 });
